7cdd5bb02ed02cf688a10ac071ec3102604d8db1
Ubuntu <ubuntu@ip-172-31-30-53.ap-northeast-1.compute.internal>
2022-01-21 04:13:56 +0000

    git add kadai-introduce.rb

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,11 +1,9 @@
-noro= "田中"
-takumi= "太郎"
-age= 25
-cub="です。"
-gub="歳"
-vub=gub+cub
-puts noro+takumi+cub
-puts age.to_s+vub
+myouji= "田中"
+name= "太郎"
+age=25
+puts myouji+ name + "です。"+ age.to_s + "歳です。"
+
+
 
 
 
